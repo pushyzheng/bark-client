c34c7d197ea895d081554a044f5af8fb7305d097
pushy <1437876073@qq.com>
2020-10-07 16:47:14 +0800

    Init project

---
+++ python/sample.py
@@ -0,0 +1,22 @@
+from bark_client import BarkClient, SoundType
+
+client = BarkClient(domain='api.day.app', key_list=['your key'])
+
+if __name__ == '__main__':
+    # Only content
+    client.push('Content')
+
+    # Title and content
+    client.push('Content', title='Title')
+
+    # Use custom sound
+    client.push('Content', title='Title', sound=SoundType.CHOO)
+
+    # Set url
+    client.push('Content', title='Title', url='https://google.com')
+
+    # Designated receiver
+    client.push('Content', title='Title', url='https://google.com', receivers=['your key'])
+
+    # Set automatically copy
+    client.push('Content', title='Title', url='https://google.com', receivers=['your key'], automatically_copy=True)
